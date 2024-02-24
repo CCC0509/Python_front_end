da58558620b0bb67d8c8ec88a8b6e9bd502f2e79
周家頡 <jackchou@zhoujiajiadeMacBook-Pro.local>
2024-02-24 13:24:49 +0800

    setting html file from flask template inheritance

--- index.py
+++ index.py
@@ -10,6 +10,16 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/cash")
+def cash_form():
+    return render_template("cash.html")
+
+
+@app.route("/stock")
+def stock_form():
+    return render_template("stock.html")
+
+
 # implement this app if it is not import from other file
 if __name__ == "__main__":
     app.run(debug=True)
