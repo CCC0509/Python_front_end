1e8581102ae92ba5bf461f21b7017437fd003dab
周家頡 <jackchou@zhoujiajiadeMacBook-Pro.local>
2024-02-25 17:11:01 +0800

    handle stock data

--- db_setting.py
+++ db_setting.py
@@ -8,7 +8,8 @@ cursor.execute(
     """create table cash (transaction_id integer primary key, taiwanese_dollars integer, us_dollars real, note varchar(30), date_info date)""")
 
 cursor.execute(
-    """create table stock (transaction_id integer primary key, stock_id varchar(10), stock_num integer, stock_price real, processing_fee integer, tax integer, date_info date)""")
+    """create table stock (transaction_id integer primary key, stock_id varchar(10), stock_num integer, stock_price real, processing_fee real, tax integer, date_info date)""")
+
 
 conn.commit()
 conn.close()
--- index.py
+++ index.py
@@ -31,9 +31,15 @@ def close_connection(exception):
 r = requests.get("https://tw.rter.info/capi.php")
 exchange_rate = r.json()["USDTWD"]["Exrate"]
 
+# get info from API
+s = requests.get(
+    "https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_AVG_ALL")
+s_info = s.json()
+
 
 @app.route("/")
 def home():
+    # print(type(s_info[0]["Code"]))
     # data from cash table
     conn = get_db()
     cursor = conn.cursor()
@@ -44,13 +50,47 @@ def home():
     for data in result_cash:
         twd += data[1]
         usd += data[2]
-    total = math.floor(twd+usd*exchange_rate)
+    cash_total = math.floor(twd+usd*exchange_rate)
     # save data as a object
     data_cash = {"twd": twd, "usd": usd, "rate": exchange_rate,
-                 "total": total, "result": result_cash}
+                 "total": cash_total, "result": result_cash}
+    # data from stock table
+    result_stock = cursor.execute("select * from stock").fetchall()
+    # list all stock_id which is no duplicate
+    stock_list = []
+    for data in result_stock:
+        if data[1] not in stock_list:
+            stock_list.append(data[1])
+    # get each stock info
+    stock_info = []
+    # all stock price
+    stock_price = 0
+    for stock in stock_list:
+        result = cursor.execute(
+            "select * from stock where stock_id=?", (stock,)).fetchall()
+        # get price which match stock_id
+        for i in s_info:
+            if i["Code"] == stock:
+                current_price = float(i["ClosingPrice"])
+        num = 0
+        stock_buy_total = 0
+        for data in result:
+            num += data[2]
+            stock_buy_total += data[2]*data[3]+data[4]+data[5]
+        stock_total = math.floor(current_price*num)
+        average_cost = round(stock_buy_total/num, 2)
+        roi = math.floor(((stock_total-stock_buy_total)/stock_buy_total)*100)
+        stock_price += stock_total
+        # store stock info we need in to stock_info
+        stock_info.append({"stock_id": stock, "num": num, "price": current_price, "total_price": stock_total,
+                          "cost": stock_buy_total, "average_cost": average_cost, "roi": roi})
+    # calculate the percentage of each stock from all stock
+    for stock in stock_info:
+        stock["value_percentage"] = round(
+            (stock["total_price"]/stock_price)*100, 2)
     # render the html file from templates folder
     # send data to html
-    return render_template("index.html", data_cash=data_cash)
+    return render_template("index.html", data_cash=data_cash, stock_info=stock_info)
 
 
 @app.route("/cash")
@@ -60,7 +100,7 @@ def cash_form():
 
 @app.route("/cash", methods=["POST"])
 def submit_cash():
-    # get values from heml form
+    # get values from html form
     twd = 0
     usd = 0
     if request.values["taiwanese-dollars"] != "":
@@ -69,7 +109,6 @@ def submit_cash():
         usd = request.values["us-dollars"]
     note = request.values["note"]
     date = request.values["date"]
-
     # update data to database
     conn = get_db()
     cursor = conn.cursor()
@@ -80,6 +119,17 @@ def submit_cash():
     return redirect("/")
 
 
+# delete data from datatype which choosen from html
+@app.route("/delete_cash", methods=["post"])
+def delete_table():
+    del_id = request.values["id"]
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("delete from cash where transaction_id =?", (del_id))
+    conn.commit()
+    return redirect("/")
+
+
 @app.route("/stock")
 def stock_form():
     return render_template("stock.html")
@@ -87,15 +137,15 @@ def stock_form():
 
 @app.route("/stock", methods=["post"])
 def submit_stock():
-    num = 0
-    price = 0
     fee = 0
     tax = 0
     stock_id = request.values["stock-id"]
     num = request.values["stock-num"]
     price = request.values["stock-price"]
-    fee = request.values["processing-fee"]
-    tax = request.values["tax"]
+    if request.values["processing-fee"] != "":
+        fee = request.values["processing-fee"]
+    if request.values["tax"] != "":
+        tax = request.values["tax"]
     date = request.values["date"]
 
     conn = get_db()
